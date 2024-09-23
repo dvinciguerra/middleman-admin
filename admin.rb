fdaf9b7386f695dec0ffcd019d9b319a25118544
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2024-09-23 19:34:48 -0300

    fix: solve new articles link and remove unused code

--- admin.rb
+++ admin.rb
@@ -546,12 +546,7 @@ __END__
     <!-- Page title actions -->
     <div class="col-auto ms-auto d-print-none">
       <div class="btn-list">
-        <!-- span class="d-none d-sm-inline">
-          <a href="#" class="btn">
-            New view
-          </a>
-        </span -->
-        <a href="#" class="btn btn-primary d-none d-sm-inline-block" data-bs-toggle="modal" data-bs-target="#modal-report">
+        <a href="/articles/new" class="btn btn-primary d-none d-sm-inline-block">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M12 5l0 14"></path><path d="M5 12l14 0"></path></svg>
           New Article
         </a>
